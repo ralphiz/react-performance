85d838a0ec9f562e7359dfe186a8adadf11d8757
Ralph Cacho <cacho.rc@gmail.com>
2022-06-12 16:04:55 -0700

    ex: consuming components

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -107,8 +107,13 @@ function Grid() {
 Grid = React.memo(Grid)
 
 function Cell({row, column}) {
-  const state = useAppState()
+  const state = useAppState
   const cell = state.grid[row][column]
+  return <CellImpl cell={cell} row={row} column={column} />
+}
+Cell = React.memo(Cell)
+
+function CellImpl({cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -124,7 +129,7 @@ function Cell({row, column}) {
     </button>
   )
 }
-Cell = React.memo(Cell)
+CellImpl = React.memo(CellImpl)
 
 function DogNameInput() {
   const [state, dispatch] = useDogState()
